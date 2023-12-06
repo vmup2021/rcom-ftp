856409c998e2af30771a7b9651ebdf92862c4dab
Luis Miguel Lima Tavares <up202108662@fe.up.pt>
2023-12-06 21:42:02 +0000

    Fix FTP download usage message

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 int main(int argc, char * argv[])
 {
     if (argc != 2) {
-        fprintf(stderr, "usage: download ftp://[<user>:<password>@]<host>/<url-path>\n");
+        fprintf(stderr, "usage: download ftp://[user:<password]@<host>/<url-path>\n");
         exit(1);
     }
 
